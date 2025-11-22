e020e629a52ce428f4a688f730f322c62ad5acf1
justoanff <toantran.co.work@gmail.com>
2025-11-22 09:40:32 +0700

    update nvim conf

--- macos/.config/nvim/lua/plugins/specs/tools.lua
+++ macos/.config/nvim/lua/plugins/specs/tools.lua
@@ -55,7 +55,7 @@ return {
     'linux-cultist/venv-selector.nvim',
     dependencies = { 'neovim/nvim-lspconfig', 'nvim-telescope/telescope.nvim', 'mfussenegger/nvim-dap-python' },
     lazy = false,
-    branch = 'regexp',
+    branch = 'main',
     config = function()
       require("plugins.configs.venv-selector")
     end,
